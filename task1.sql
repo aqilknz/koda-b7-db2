e1e8e3d71b3196865345dcc6eff9ef773b63beca
aqilknz <khairunnz123@gmail.com>
2026-05-20 13:19:03 +0700

    feat: add a self join implementation exercise

--- task1.sql
+++ task1.sql
@@ -12,14 +12,26 @@ INSERT INTO customers(name, referred_by) VALUES
 ('Eva', 2),
 ('Frank', 3);
 
+-- 1
 SELECT c1.name AS "customer_name", c2.name AS "referrer_name"
 FROM customers c1
 LEFT JOIN customers c2 on c1.referred_by = c2.id;
 
+-- 2
 SELECT name AS "Customer_Name"
 FROM customers
 WHERE referred_by IS NULL;
 
-
-SELECT c1.name AS "customer_name"
+-- 3
+SELECT c1.name AS "customer_name", count(c2.id) AS "Count"
 FROM customers c1
+LEFT JOIN customers c2 ON c1.id = c2.referred_by
+GROUP BY c1.name;
+
+-- 4
+SELECT c1.name AS "customer_name", count(c2.id) AS "Count"
+FROM customers c1
+JOIN customers c2 ON c1.id = c2.referred_by
+GROUP BY c1.name;
+
+
